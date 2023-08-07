4708110d8aac3f6be7c4285f86bee5d3fd6a8cc1
Ajmain Nishu <ajmainnishu@gmail.com>
2023-08-08 01:22:43 +0600

    feat: React toastify added

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,6 +1,8 @@
 import './Articles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Articles = (props) => {
     const {title, cover_image, read_time, publish_date, author_name, author_image} = props.blogs;
@@ -19,6 +21,7 @@ const Articles = (props) => {
                 </div>
                 <div className='article-read'>
                     <p>{read_time} min read &nbsp; <FontAwesomeIcon onClick={() => totalBookmark(title)} style={{cursor: 'pointer'}} icon={faBookmark} /></p>
+                    <ToastContainer autoClose={2000} />
                 </div>
             </div>
             <h2>{title}</h2>
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,11 +3,14 @@ import './Blogs.css'
 import { useEffect } from 'react';
 import Articles from '../Articles/Articles';
 import Bookmark from '../Bookmark/Bookmark';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
     const [times, setTimes] = useState([]);
     const [bookmarks, setBookmarks] = useState([]);
+    // const notify = () => toast("Wow so easy!");
     useEffect(() => {
         fetch('./fakeData/fakeData.json')
         .then(res => res.json())
@@ -18,11 +21,13 @@ const Blogs = () => {
         setTimes(newTime)
     }
     const totalBookmark = items => {
+        // toast("Can't add more than 1");
         let newBookmark = [];
         const exists = bookmarks.find(bookmarkItem => bookmarkItem === items);
         if (!exists) {
             newBookmark = [...bookmarks, items];
         } else {
+            toast.warn("Can't add more than 1");
             const remaining = bookmarks.filter(bookmarkItem => bookmarkItem !== items);
             newBookmark = [...remaining, exists];
         }
@@ -30,6 +35,7 @@ const Blogs = () => {
     }
     return (
         <div className='blogs-container'>
+
             <div className='article-container'>
                 {
                     blogs.map(blog => <Articles blogs={blog} key={blog.id} totalTime={totalTime} totalBookmark={totalBookmark}></Articles>)
@@ -38,6 +44,8 @@ const Blogs = () => {
             <div>
                 <Bookmark times={times} bookmarks={bookmarks}></Bookmark>
             </div>
+            {/* <button onClick={notify}>Notify!</button>
+        <ToastContainer /> */}
         </div>
     );
 };
